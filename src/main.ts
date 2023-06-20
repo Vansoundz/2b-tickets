735465707e0098c2aaddfd4158bea1dbde2f640d
vansoundz <thebeatplanet.silente@gmail.com>
2023-06-20 07:27:24 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('2brother ticktets')
     .setDescription('Documentation for 2brother ticktets')
